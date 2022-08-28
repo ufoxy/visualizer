aa624380c3f5966cc6f27c97be9c074460b5d1ac
foxyuh <arthurbryansk@gmail.com>
2022-08-28 00:14:13 -0300

    inicio da dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,33 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <React.Fragment>
-      <div>oi</div>
+      <div style={{
+        backgroundColor: '#0B132B',
+        margin: '5px 0 5px 5px',
+        maxWidth: '300px',
+        height: 'calc(100vh - 10px)',
+        padding: '8px',
+        fontSize: '24',
+        color: '#3A506B'
+      }}>
+        <h1 style={{}}>
+          Hello World!
+        </h1>
+
+        <ul 
+        style={{
+          listStyleType: 'none',
+          color: '#FFF'
+        }}
+        >
+          <li>oi</li>
+          <li>oi</li>
+          <li>oi</li>
+          <li>oi</li>
+          <li>oi</li>
+        </ul>
+
+      </div>
     </React.Fragment>
   )
 }
