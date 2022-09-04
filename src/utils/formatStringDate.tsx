7e9b46b7da2037459d1e1e65ea13f861bedd02b8
foxyuh <arthurbryansk@gmail.com>
2022-09-03 22:27:39 -0300

    import dos utils criados

---
+++ src/utils/formatStringDate.tsx
@@ -0,0 +1,6 @@
+function formatStringDate(data:any) {
+    let date = new Date(data)
+    return `${date.toLocaleDateString("pt-BR")} ${date.toLocaleTimeString("pt-BR")} `
+}
+
+export default formatStringDate;
