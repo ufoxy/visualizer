d7941f12d84c4eb43f63a29651a6728862fbc85e
foxyuh <arthurbryansk@gmail.com>
2022-09-10 16:33:30 -0300

    refatoração e estilização da dashboard e do menu lateral

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -19,73 +19,80 @@ function Dashboard({
 }: any) {
   const { query } = useRouter();
 
-  // console.log(equipment);
-  // console.log(equipmentModel);
-  // console.log(equipmentPositionHistory);
-  // console.log(equipmentStateHistory);
-  // console.log(equipmentState);
+  const model = useGetModel(
+    query.id, 
+    equipment, 
+    equipmentModel
+    ).map((e:any) => e.name)
+    // Modelo do equipamento
+  const productivityPercentage = useGetProductivityPercentageById(
+    query.id,
+    equipment,
+    equipmentModel,
+    equipmentStateHistory
+  ).replace(".", ",")
+    // Percentual de produtividade usando a fórmula a seguir: (Total de operações / 24) * 100
+  const earnings = useGetEarningCalculatorById(
+    query.id,
+    equipmentModel,
+    equipmentStateHistory,
+    equipment
+  ).toFixed(2).replace(".", ",")
+    // Ganhos gerados no dia atual
+  const position = useGetPositionById(
+    query.id,
+    equipmentPositionHistory
+  ).map((e: any) => `${e.position.lat}, ${e.position.lon}`)
+    // Posição atual do equipamento em latitude e longitude.
+  const positionLastAtt = useGetPositionById(
+    query.id,
+    equipmentPositionHistory
+  ).map((e: any) => formatStringDate(e.date))
+    // Última atualização de posição do equipamento em data e horário.
+  const stateColor = useGetStatusClassById(
+    query.id,
+    equipmentStateHistory,
+    equipmentState
+  )
+    // className com as alterações de cor de cada tipo de estado. ex; Vermelho para parado.
+  const state = useGetStateById(
+    query.id,
+    equipmentStateHistory,
+    equipmentState
+  ).map((e: any) => e.state.map((e: any) => e.name))
+    // Estaddo atual do equipamento. ex; Parado.
+  const stateLastAtt = useGetStateById(
+    query.id,
+    equipmentStateHistory,
+    equipmentState
+  ).map((e: any) => formatStringDate(e.date))
+    // Última atualização de estado do equipamento em data e horário.
 
   return (
     <React.Fragment>
       <section className={styles.section}>
+        <h2>{`Nome: ${query.name}`}</h2>
+        <h3>{`Modelo: ${model}`}</h3>
+
         <div className={styles.flex}>
-          <p>
-            {`Percentual de Produtividade: ${useGetProductivityPercentageById(
-              query.id,
-              equipment,
-              equipmentModel,
-              equipmentStateHistory
-            ).replace(".", ",")}%`}
-          </p>
-          <p>
-            {`Ganhos Gerados: R$${useGetEarningCalculatorById(
-              query.id,
-              equipmentModel,
-              equipmentStateHistory,
-              equipment
-            )
-              .toFixed(2)
-              .replace(".", ",")}`}
-          </p>
+          <p>{`Percentual de Produtividade: ${productivityPercentage}%`}</p>
+          <p>{`Ganhos Gerados: R$${earnings}`}</p>
         </div>
         <div className={styles.flex}>
-          <p>
-            {`Posição Atual: ${useGetPositionById(
-              query.id,
-              equipmentPositionHistory
-            ).map((e: any) => `${e.position.lat}, ${e.position.lon}`)}`}
-          </p>
-          <p>
-            {`Última Atualização: ${useGetPositionById(
-              query.id,
-              equipmentPositionHistory
-            ).map((e: any) => formatStringDate(e.date))}`}
-          </p>
+          <p>{`Posição Atual: ${position}`}</p>
+          <p>{`Última Atualização: ${positionLastAtt}`}</p>
         </div>
         <div className={styles.flex}>
           <p>
             {`Status: `}
             <span
-              className={`circle-dot ${useGetStatusClassById(
-                query.id,
-                equipmentStateHistory,
-                equipmentState
-              )}`}
+              className={`circle-dot ${stateColor}`}
             ></span>
-            {`${useGetStateById(
-              query.id,
-              equipmentStateHistory,
-              equipmentState
-            ).map((e: any) => e.state.map((e: any) => e.name))}`}
+            {`${state}`}
           </p>
-          <p>
-            {`Última Atualização: ${useGetStateById(
-              query.id,
-              equipmentStateHistory,
-              equipmentState
-            ).map((e: any) => formatStringDate(e.date))}`}
-          </p>
+          <p>{`Última Atualização: ${stateLastAtt}`}</p>
         </div>
+
         <h1>o id é: {query.id}</h1>
       </section>
     </React.Fragment>
--- src/components/MenuLateral.tsx
+++ src/components/MenuLateral.tsx
@@ -3,14 +3,25 @@ import Link from "next/link";
 
 import styles from "../../styles/components/Menu-lateral.module.scss"
 
-function MenuLateral({ children }: any) {
+function MenuLateral({ equipment }: any) {
+  console.log(equipment)
   return (
     <React.Fragment>
       <section className={styles.section}>
         <h1 style={{}}>Hello World!</h1>
-        <ul className={styles.ul}>
-          {children}
-        </ul>
+
+        <div className={styles.scroll_navbar}>
+          <ul className={styles.ul}>
+            {equipment.map((e: any) => (
+              <Link key={e.id} href={`${e.name}?id=${e.id}`} prefetch={false}>
+                <li key={e.id} className={styles.li}>
+                  {e.name}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+
         <Link href="/">
           <button className={styles.button}>
             Mapa
