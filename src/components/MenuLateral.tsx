c93d7308e026b7ed8cb663c8ab90e59d4808554b
foxyuh <arthurbryansk@gmail.com>
2022-09-10 11:35:47 -0300

    estilização do menu lateral com scss

--- src/components/MenuLateral.tsx
+++ src/components/MenuLateral.tsx
@@ -1,46 +1,22 @@
 import React, { Children } from "react";
 import Link from "next/link";
 
+import styles from "../../styles/components/Menu-lateral.module.scss"
+
 function MenuLateral({ children }: any) {
   return (
     <React.Fragment>
-      <div
-        style={{
-          backgroundColor: "#0B132B",
-          margin: "5px 5px 5px 5px",
-          minWidth: "300px",
-          maxWidth: "300px",
-          height: "calc(100vh - 10px)",
-          fontSize: "24",
-          color: "#3A506B",
-          position: 'relative',
-          overflow: 'hidden'
-        }}
-      >
+      <section className={styles.section}>
         <h1 style={{}}>Hello World!</h1>
-        <ul
-          style={{
-            listStyleType: "none",
-            padding: "8px",
-            color: "#FFF",
-          }}
-        >
+        <ul className={styles.ul}>
           {children}
         </ul>
         <Link href="/">
-            <button
-              style={{
-                minWidth: "calc(300px - 16px)",
-                minHeight: "25px",
-                margin: '0 8px 25px 8px',
-                position: 'absolute',
-                bottom: '0'
-              }}
-            >
-              Mapa
-            </button>
+          <button className={styles.button}>
+            Mapa
+          </button>
         </Link>
-      </div>
+      </section>
     </React.Fragment>
   );
 }
