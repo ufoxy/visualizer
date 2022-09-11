30a4e64ae9262670aca2475d11dbcbb8ce476408
foxyuh <arthurbryansk@gmail.com>
2022-09-11 15:15:24 -0300

    adicionando marcadores e popup

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,30 +2,58 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
-import styles from '../../styles/components/Map.module.scss'
+import styles from "../../styles/components/Map.module.scss";
+import useGetPosition from "../hooks/getPosition";
+import formatStringDate from "../utils/formatStringDate";
+import Link from "next/link";
+import useGetModel from "../hooks/getModel";
+import useGetStateById from "../hooks/getStateById";
+import useGetStatusClassById from "../hooks/getStatusClassById";
 
-const DEFAULT_CENTER = { lat: -19.151801, lon: -46.007759 }
+const DEFAULT_CENTER = { lat: -19.151801, lon: -46.007759 };
 
 // Basic: https://api.mapbox.com/styles/v1/ufoxy/cl7xjsprk008t15lgay2bk8gf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidWZveHkiLCJhIjoiY2w3d2hsOTlsMGhvNTN2b2F5bHlhNGU2bSJ9.ux0VWarP69sXVXtiHXOjkw
 // Outdoors: https://api.mapbox.com/styles/v1/ufoxy/cl7xj9uzv003m14r3sxlfu4ni/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidWZveHkiLCJhIjoiY2w3d2hsOTlsMGhvNTN2b2F5bHlhNGU2bSJ9.ux0VWarP69sXVXtiHXOjkw
 // Dark: https://api.mapbox.com/styles/v1/ufoxy/cl7xjyti800by15ryoins5lis/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidWZveHkiLCJhIjoiY2w3d2hsOTlsMGhvNTN2b2F5bHlhNGU2bSJ9.ux0VWarP69sXVXtiHXOjkw
 
-const Map = () => {
-    return (
-        <MapContainer
-            center={[DEFAULT_CENTER.lat, DEFAULT_CENTER.lon]}
-            zoom={14}
-            scrollWheelZoom={false}
-            className={styles.map}
-        >
-            <TileLayer
-                url="https://api.mapbox.com/styles/v1/ufoxy/cl7xj9uzv003m14r3sxlfu4ni/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidWZveHkiLCJhIjoiY2w3d2hsOTlsMGhvNTN2b2F5bHlhNGU2bSJ9.ux0VWarP69sXVXtiHXOjkw"
-            />
-            <Marker position={[40.8054, -74.0241]} draggable={true}>
-                <Popup>Hey ! I live here</Popup>
-            </Marker>
-        </MapContainer>
-    );
+const Map = ({
+  equipment,
+  equipmentPositionHistory,
+  equipmentModel
+}: any) => {
+  return (
+    <MapContainer
+      center={[DEFAULT_CENTER.lat, DEFAULT_CENTER.lon]}
+      zoom={14}
+      scrollWheelZoom={false}
+      className={styles.map}
+    >
+      <TileLayer url="https://api.mapbox.com/styles/v1/ufoxy/cl7xj9uzv003m14r3sxlfu4ni/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidWZveHkiLCJhIjoiY2w3d2hsOTlsMGhvNTN2b2F5bHlhNGU2bSJ9.ux0VWarP69sXVXtiHXOjkw" />
+      {useGetPosition(equipmentPositionHistory).map((e: any) => {
+        const id = e.id;
+        const equipmentName = equipment.find((x: any) => x.id === e.id).name;
+        const lastUpdate = formatStringDate(new Date(e.position.date));
+        const model = useGetModel(id, equipment, equipmentModel)
+          .map((e: any) => e.name)
+          .join("");
+        return (
+          <Marker position={[e.position.lat, e.position.lon]} key={id}>
+            <Popup>
+              <p>{`Name: ${equipmentName}`}</p>
+              <p>{`Modelo: ${model}`}</p>
+              <p>{`Ultima atualização: ${lastUpdate}`}</p>
+              <Link href={`equipamento/${equipmentName}?id=${id}`}>
+                <button>Ver detalhe</button>
+              </Link>
+            </Popup>
+          </Marker>
+        );
+      })}
+      <Marker position={[-19.151801, -46.007759]} draggable={false}>
+        <Popup>Hey ! I live here</Popup>
+      </Marker>
+    </MapContainer>
+  );
 };
 
 export default Map;
