05bfa6318059042823044cddb6cad6a913b36b04
foxyuh <arthurbryansk@gmail.com>
2022-09-16 21:48:15 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,7 @@ import { NextPage } from 'next'
 import axios from 'axios'
 import MenuLateral from '../src/components/MenuLateral'
 import dynamic from "next/dynamic";
-import Link from 'next/link'
-
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
-import useGetPosition from '../src/hooks/getPosition'
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
-// import Map from '../src/components/Map'
 
 const DEFAULT_CENTER = [38.907132, -77.036546]
 
@@ -56,21 +49,11 @@ const Home: NextPage = ({ equipment, equipmentPositionHistory, equipmentModel }:
 
   return (
     <React.Fragment>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <h1 style={{
-          position: 'absolute',
-          right: '2%',
-          zIndex: '99'
-        }}>teste</h1>
+      <section className={styles.section}>
+        <h1 className={styles.h1}>teste</h1>
         <MenuLateral equipment={equipment} path={'equipamento/'} />
-
         <MapCaller equipment={equipment} equipmentPositionHistory={equipmentPositionHistory} equipmentModel={equipmentModel} />
-      </div>
+      </section>
     </React.Fragment>
   )
 }
