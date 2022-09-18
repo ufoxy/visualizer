80d94c9e4fe033ea3126fadde21fb48f1c3fced8
foxyuh <arthurbryansk@gmail.com>
2022-09-18 19:57:51 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
           <title>Visualizer</title>
         </Head>
         <body>
