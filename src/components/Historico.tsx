07e2447e7e5aec9b5a7d1c8c30c26dba7643c0bf
foxyuh <arthurbryansk@gmail.com>
2022-09-24 01:17:57 -0300

    update no historico

--- src/components/Historico.tsx
+++ src/components/Historico.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Tabs from "./Tabs/Tabs";
-import styles from "../../styles/components/Historico.module.scss"
+import styles from "../../styles/components/Historico.module.scss";
+import Image from "next/image";
+const gif = require("../assets/gifs/load-animation.gif") as string;
+import { TypeAnimation } from 'react-type-animation';
 
 function Historico() {
   const { query } = useRouter();
@@ -11,12 +14,28 @@ function Historico() {
       <section className={styles.section}>
         <Tabs />
         <div className={styles.container}>
-            <h1 className={styles.h1}>
-                Em desenvolvimento
-                <span>
-                    <img src="" alt="" />
-                </span>
-            </h1>
+          <h1 className={styles.h1}>
+            Em desenvolvimento
+            <span>
+              <TypeAnimation
+                cursor={false}
+                sequence={[
+                  ".",
+                  1000,
+                  "..",
+                  1000,
+                  "...",
+                  1000,
+                  "",
+                  1000,
+                ]}
+                speed={99}
+                wrapper="h2"
+                repeat={Infinity}
+                style={{fontSize:"30px"}}
+              />
+            </span>
+          </h1>
         </div>
       </section>
     </React.Fragment>
