31234e6dd48077b3aa2acbdf9d5a05daa67fba81
foxyuh <arthurbryansk@gmail.com>
2022-09-24 16:37:18 -0300

    corrigindo path de arquivo

--- src/components/Detalhes.tsx
+++ src/components/Detalhes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Tabs from "./Tabs/Tabs";
-import styles from "../../styles/components/Historico.module.scss";
+import styles from "../../styles/components/Detalhes.module.scss";
 import Developing from "./Developing";
 
 function Detalhes() {
