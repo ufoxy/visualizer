76670a7e45e12901f38bcd22f65163368f329ef3
foxyuh <arthurbryansk@gmail.com>
2022-09-24 16:29:54 -0300

    Create Developing.tsx

---
+++ src/components/Developing.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useRouter } from "next/router";
+import styles from "../../styles/components/Developing.module.scss";
+import { TypeAnimation } from "react-type-animation";
+
+function Developing() {
+  return (
+    <React.Fragment>
+      <article className={styles.container}>
+        <h1 className={styles.h1}>
+          Em desenvolvimento
+          <span>
+            <TypeAnimation
+              cursor={false}
+              sequence={[".", 1000, "..", 1000, "...", 1000, "", 1000]}
+              speed={99}
+              wrapper="h2"
+              repeat={Infinity}
+              style={{ fontSize: "30px" }}
+            />
+          </span>
+        </h1>
+      </article>
+    </React.Fragment>
+  );
+}
+
+export default Developing;
