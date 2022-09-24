c019b12fa500e4f3b69dade15c9aa210cd131611
foxyuh <arthurbryansk@gmail.com>
2022-09-24 16:37:22 -0300

    Update Developing.tsx

--- src/components/Developing.tsx
+++ src/components/Developing.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useRouter } from "next/router";
 import styles from "../../styles/components/Developing.module.scss";
 import { TypeAnimation } from "react-type-animation";
 
