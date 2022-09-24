9358f085f4f8eb2d7a214f76f3d4b779fa2aa069
foxyuh <arthurbryansk@gmail.com>
2022-09-23 23:31:25 -0300

    estilização da tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,6 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import styles from "../../../styles/components/Tabs/Tabs.module.scss"
+import { useRouter } from "next/router";
+
 function Tabs() {
+
+  const { query } = useRouter();
+
   return (
-    <></>
+    <React.Fragment>
+      <article className={styles.tabs_div}>
+      <nav>
+        <ul className={styles.nav_ul}>
+          <li className={styles.li}>
+            <Link href={`/equipamento/${query.name}?id=${query.id}`}>{query.name}</Link>
+          </li>
+          <li className={styles.li}>
+            <Link href={`historico/${query.name}?id=${query.id}`}>Histórico</Link>
+          </li>
+        </ul>
+      </nav>
+        </article>
+    </React.Fragment>
   );
 }
 
