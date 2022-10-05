ddf38f34248023e344555101270253079964bbf4
foxyuh <arthurbryansk@gmail.com>
2022-10-05 18:14:45 -0300

    Update Search-bar.tsx

--- src/components/Search-bar/Search-bar.tsx
+++ src/components/Search-bar/Search-bar.tsx
@@ -1,7 +1,19 @@
+import { useContext, useState } from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import styles from "../../../styles/components/Search-bar/Search-bar.module.scss";
+import MenuLateralContext from "../../common/contexts/Menu-lateral";
 
 function SearchBar() {
+  const { ContextFunction }: any = useContext(MenuLateralContext);
+  // ContextFunction("Funciona!!!!");
+
+  function getSearch(e: any) {
+    const value = e.target.value;
+    setSearchValue(value);
+  };
+
+  const [searchValue, setSearchValue] = useState("");
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.searchBar}>
@@ -10,8 +22,11 @@ function SearchBar() {
           type="text"
           name="searchQueryInput"
           placeholder="Pesquisar"
+          onChange={getSearch}
         />
-        <button className={styles.button}><BiSearchAlt fontSize={22}/></button>
+        <button className={styles.button}>
+          <BiSearchAlt fontSize={22} />
+        </button>
       </div>
     </div>
   );
