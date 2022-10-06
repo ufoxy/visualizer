a6e3b51f8aca0d66d44efcfec9d7ef0c4b13a620
foxyuh <arthurbryansk@gmail.com>
2022-10-05 21:13:35 -0300

    adicionando icon ao botão do mapa

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import { ImMap } from "react-icons/im";
 import styles from "../../../styles/components/Button/Button.module.scss";
 
 function Button({ children }: any) {
   return (
     <Link href="/">
-      <button className={styles.button}>{children}</button>
+      <button className={styles.button}>
+        {children}
+        <ImMap fontSize={20} />
+      </button>
     </Link>
   );
 }
