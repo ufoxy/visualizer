be0fb85c67e29d3c852d92a1f28909d8b17da39a
foxyuh <arthurbryansk@gmail.com>
2022-10-26 21:44:44 -0300

    Update Size-block.tsx

--- src/components/Size-block.tsx
+++ src/components/Size-block.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { SiGithub } from "react-icons/si";
 import styles from "../../styles/components/Size-block.module.scss";
 
 function SizeBlock() {
@@ -13,6 +14,29 @@ function SizeBlock() {
           Por favor, entre em um dispositivo com uma resolução mínima de
           940x600.
         </p>
+        <footer className={styles.footer}>
+          <a
+            href="https://github.com/ufoxy/visualizer"
+            target="_blank"
+            rel="noreferrer"
+            className={styles.a}
+          >
+            Visualizer
+          </a>
+          <a
+            href="https://github.com/ufoxy/visualizer"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i>
+              <SiGithub
+                fontSize={28}
+                color="white"
+                style={{ paddingTop: "3px" }}
+              ></SiGithub>
+            </i>
+          </a>
+        </footer>
       </section>
     </React.Fragment>
   );
