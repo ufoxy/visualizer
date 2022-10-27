3632e6255c4ed387807093ca4766caa3159a6281
foxyuh <arthurbryansk@gmail.com>
2022-10-26 21:44:41 -0300

    Update Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,7 @@
 import LeafletMap from "./Leaflet-map/Leaflet-map";
 import styles from "../../styles/components/Map.module.scss";
 import Link from "next/link";
+import { SiGithub } from "react-icons/si";
 
 const Map = () => {
   return (
@@ -14,10 +15,13 @@ const Map = () => {
           target="_blank"
           rel="noreferrer"
         >
-          <i
-            className="fa fa-github"
-            style={{ fontSize: "32px", color: "white" }}
-          ></i>
+          <i>
+            <SiGithub
+              fontSize={28}
+              color="white"
+              style={{ paddingTop: "3px" }}
+            ></SiGithub>
+          </i>
         </a>
       </div>
       <LeafletMap />
