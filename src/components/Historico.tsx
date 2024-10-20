33240d0ea6b0ed0c9888abc19d0479e46da1b57f
Arthur Bryan <arthurbryansk@gmail.com>
2024-10-20 19:14:19 -0300

    Update Historico.tsx

--- src/components/Historico.tsx
+++ src/components/Historico.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Tabs from "./Tabs/Tabs";
 import styles from "../../styles/components/Historico.module.scss";
-import Developing from "./Developing";
 import dynamic from "next/dynamic";
 
 function Historico() {
@@ -16,7 +15,6 @@ function Historico() {
     <React.Fragment>
       <section className={styles.section}>
         <Tabs />
-        {/* <Developing /> */}
         <Map />
       </section>
     </React.Fragment>
