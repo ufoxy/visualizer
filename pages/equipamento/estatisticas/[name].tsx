469a5e46291e94361e7b1ebd557cd662a81cae89
Arthur Bryan <arthurbryansk@gmail.com>
2024-12-08 22:05:21 -0300

    Produção do componente Estatísticas.

--- pages/equipamento/estatisticas/[name].tsx
+++ pages/equipamento/estatisticas/[name].tsx
@@ -6,6 +6,7 @@ import Estatisticas from "../../../src/components/Estatisticas";
 import MenuLateralContext from "../../../src/common/contexts/Menu-lateral";
 import Head from "next/head";
 import SizeBlock from "../../../src/components/Size-block";
+import StatisticsContext from "../../../src/common/contexts/Statistics";
 
 export async function getStaticPaths() {
   return {
@@ -19,14 +20,38 @@ export async function getStaticProps() {
     .get("https://visualizer-blue.vercel.app/api/equipment/")
     .then((e) => e.data);
 
+  const equipmentModel = await axios
+    .get("https://visualizer-blue.vercel.app/api/equipmentModel/")
+    .then((e) => e.data);
+
+  const equipmentPositionHistory = await axios
+    .get("https://visualizer-blue.vercel.app/api/equipmentPositionHistory/")
+    .then((e) => e.data);
+
+  const equipmentStateHistory = await axios
+    .get("https://visualizer-blue.vercel.app/api/equipmentStateHistory/")
+    .then((e) => e.data);
+
+  const equipmentState = await axios
+    .get("https://visualizer-blue.vercel.app/api/equipmentState/")
+    .then((e) => e.data);
+
   return {
     props: {
       equipment,
+      equipmentModel,
+      equipmentPositionHistory,
+      equipmentStateHistory,
+      equipmentState,
     },
   };
 }
 
-const Produto: NextPage = ({ equipment }: any) => {
+const Produto: NextPage = ({ equipment,
+  equipmentModel,
+  equipmentPositionHistory,
+  equipmentStateHistory,
+  equipmentState }: any) => {
   const path = "";
   const [filteredEquipment, setFilteredEquipment] = useState([]);
   function searchFilter(filterValue: string) {
@@ -58,7 +83,17 @@ const Produto: NextPage = ({ equipment }: any) => {
         >
           <MenuLateral />
         </MenuLateralContext.Provider>
-        <Estatisticas />
+        <StatisticsContext.Provider
+          value={{
+            equipment,
+            equipmentModel,
+            equipmentPositionHistory,
+            equipmentStateHistory,
+            equipmentState,
+          }}
+        >
+          <Estatisticas />
+        </StatisticsContext.Provider>
       </div>
     </>
   );
